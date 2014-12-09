4cd9f246a4f266735e5daed3a1cc6e94334dd942
TamChen <tam7@qq.com>
2014-12-09 21:39:59 +0800

    add part model

--- src/main/java/edu/csust/volunteer/model/User.java
+++ src/main/java/edu/csust/volunteer/model/User.java
@@ -17,118 +17,118 @@ import javax.persistence.TemporalType;
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private Long id;
-	private String username;
-	private String userno;
+	private int id;
+	private String userName;
+	private String userNo;
 	private String password;
-	private String userhead;
-	private int usersex;
-	private int usergrade;
-	private Date userbir;
-	private String userentity;
-	private String usertel;
-	private String usermail;
-	private String useraddress;
+	private String userHead;
+	private int userSex;
+	private int userGrade;
+	private Date userBir;
+	private String userEntity;
+	private String userTel;
+	private String userMail;
+	private String userAddress;
 
 	@Column(name = "UGRADE", length = 100, nullable = false)
 	public int getUsergrade() {
-		return usergrade;
+		return userGrade;
 	}
 
 	public void setUsergrade(int usergrade) {
-		this.usergrade = usergrade;
+		this.userGrade = usergrade;
 	}
 
 	@Column(name = "UIDENTITY", length = 100, nullable = false)
 	public String getUserentity() {
-		return userentity;
+		return userEntity;
 	}
 
 	public void setUserentity(String userentity) {
-		this.userentity = userentity;
+		this.userEntity = userentity;
 	}
 
 	@Column(name = "UTEL", length = 50, nullable = false)
 	public String getUsertel() {
-		return usertel;
+		return userTel;
 	}
 
 	public void setUsertel(String usertel) {
-		this.usertel = usertel;
+		this.userTel = usertel;
 	}
 
 	@Column(name = "UMAIL", length = 50, nullable = false)
 	public String getUsermail() {
-		return usermail;
+		return userMail;
 	}
 
 	public void setUsermail(String usermail) {
-		this.usermail = usermail;
+		this.userMail = usermail;
 	}
 
 	@Column(name = "UADRESS", length = 200, nullable = false)
 	public String getUseraddress() {
-		return useraddress;
+		return userAddress;
 	}
 
 	public void setUseraddress(String useraddress) {
-		this.useraddress = useraddress;
+		this.userAddress = useraddress;
 	}
 
 	@Column(name = "UHEAD", length = 100, unique = true, nullable = false)
 	public String getUserhead() {
-		return userhead;
+		return userHead;
 	}
 
 	public void setUserhead(String userhead) {
-		this.userhead = userhead;
+		this.userHead = userhead;
 	}
 	
 	@Column(name = "USEX", length = 100, unique = true, nullable = false)
 	public int getUsersex() {
-		return usersex;
+		return userSex;
 	}
 
 	public void setUsersex(int usersex) {
-		this.usersex = usersex;
+		this.userSex = usersex;
 	}
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "UBIR")
 	public Date getUserbir() {
-		return userbir;
+		return userBir;
 	}
 
 	public void setUserbir(Date userbir) {
-		this.userbir = userbir;
+		this.userBir = userbir;
 	}
 
 	@Id  
 	@GeneratedValue(strategy=GenerationType.IDENTITY) 
-	public Long getId() {
+	public int getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 
-	@Column(name = "UNAME", length = 100, unique = true, nullable = false)
+	@Column(name = "UNAME", length = 100, nullable = false)
 	public String getUsername() {
-		return username;
+		return userName;
 	}
 
 	public void setUsername(String username) {
-		this.username = username;
+		this.userName = username;
 	}
 	
 	@Column(name = "UNO", length = 100, unique = true, nullable = false)
 	public String getUserno() {
-		return userno;
+		return userNo;
 	}
 
 	public void setUserno(String userno) {
-		this.userno = userno;
+		this.userNo = userno;
 	}
 	
 	@Column(name = "UPASSWORD", length = 32, nullable = false)
